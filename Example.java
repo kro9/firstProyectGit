f397d39359a5aa559279ce72d843ca9dfc250a25
Carolina Castillo <kro9@msn.com>
2019-03-26 15:39:07 -0500

    Se añade archivo java

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example{
+	public static void main (String args[]){
+	 long max = 3123456789L;
+	 System.out.println(Long.MAX_VALUE);
+	 
+	 int num = null;
+	 Integer num2 = null;
+	}
+
+}
